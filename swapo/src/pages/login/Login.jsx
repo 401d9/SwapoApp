9c76bd2b05b6b5c3f3146f99289ac8fa53297bc5
asharabuhelwh <ashar.mohamad@yahoo.com>
2021-07-26 22:51:11 +0300

    UI design

--- swapo/src/pages/login/Login.jsx
+++ swapo/src/pages/login/Login.jsx
@@ -1,4 +1,6 @@
 import { useContext, useRef } from "react";
+import { Link } from 'react-router-dom';
+
 import "./login.css";
 import { loginCall } from "../../loginCall";
 import { AuthContext } from "../../context/AuthContext";
@@ -19,21 +21,21 @@ export default function Login() {
   };
 
   return (
-    // <div className="login">
-    //   <div className="loginWrapper">
-    //     <div className="loginLeft">
-    //       <h3 className="loginLogo">Swapo</h3>
-    //       <span className="loginDesc">
+    // <div classNameName="login">
+    //   <div classNameName="loginWrapper">
+    //     <div classNameName="loginLeft">
+    //       <h3 classNameName="loginLogo">Swapo</h3>
+    //       <span classNameName="loginDesc">
     //         Web app that connects many service providers and those who give their services to each other by swap their knowledge and expertise.
     //       </span>
     //     </div>
-    //     <div className="loginRight">
-    //       <form className="loginBox" onSubmit={handleClick}>
+    //     <div classNameName="loginRight">
+    //       <form classNameName="loginBox" onSubmit={handleClick}>
     //         <input
     //           placeholder="Username"
     //           type="text"
     //           required
-    //           className="loginInput"
+    //           classNameName="loginInput"
     //           ref={username}
     //         />
     //         <input
@@ -41,17 +43,17 @@ export default function Login() {
     //           type="password"
     //           required
     //           minLength="6"
-    //           className="loginInput"
+    //           classNameName="loginInput"
     //           ref={password}
     //         />
-    //         <button className="loginButton" type="submit" disabled={isFetching}>
+    //         <button classNameName="loginButton" type="submit" disabled={isFetching}>
     //           {isFetching ? (
     //             <CircularProgress color="white" size="20px" />
     //           ) : (
     //             "Log In"
     //           )}
     //         </button>
-    //         <button className="loginRegisterButton">
+    //         <button classNameName="loginRegisterButton">
     //           {isFetching ? (
     //             <CircularProgress color="white" size="20px" />
     //           ) : (
@@ -66,46 +68,46 @@ export default function Login() {
 
     ///
     // <div>
-    //   <div class="container">
-    //     <div class="d-flex justify-content-center h-100">
-    //       <div class="card">
-    //         <div class="card-header">
+    //   <div className="container">
+    //     <div className="d-flex justify-content-center h-100">
+    //       <div className="card">
+    //         <div className="card-header">
     //           <h3>Sign In</h3>
-    //           <div class="d-flex justify-content-end social_icon">
-    //             <span><i class="fab fa-facebook-square"></i></span>
-    //             <span><i class="fab fa-google-plus-square"></i></span>
-    //             <span><i class="fab fa-twitter-square"></i></span>
+    //           <div className="d-flex justify-content-end social_icon">
+    //             <span><i className="fab fa-facebook-square"></i></span>
+    //             <span><i className="fab fa-google-plus-square"></i></span>
+    //             <span><i className="fab fa-twitter-square"></i></span>
     //           </div>
     //         </div>
-    //         <div class="card-body">
+    //         <div className="card-body">
     //           <form onSubmit={handleClick}>
-    //             <div class="input-group form-group">
-    //               <div class="input-group-prepend">
-    //                 <span class="input-group-text"><i class="fas fa-user"></i></span>
+    //             <div className="input-group form-group">
+    //               <div className="input-group-prepend">
+    //                 <span className="input-group-text"><i className="fas fa-user"></i></span>
     //               </div>
-    //               <input type="text" class="form-control" placeholder="username" required ref={username} />
+    //               <input type="text" className="form-control" placeholder="username" required ref={username} />
 
     //             </div>
-    //             <div class="input-group form-group">
-    //               <div class="input-group-prepend">
-    //                 <span class="input-group-text"><i class="fas fa-key"></i></span>
+    //             <div className="input-group form-group">
+    //               <div className="input-group-prepend">
+    //                 <span className="input-group-text"><i className="fas fa-key"></i></span>
     //               </div>
-    //               <input type="password" class="form-control" placeholder="password" minLength="6" ref={password} />
+    //               <input type="password" className="form-control" placeholder="password" minLength="6" ref={password} />
     //             </div>
-    //             <div class="row align-items-center remember">
+    //             <div className="row align-items-center remember">
     //               <input type="checkbox" />Remember Me
     //             </div>
-    //             <div class="form-group">
-    //               <input type="submit" value="Login" class="btn float-right login_btn" disabled={isFetching} />
+    //             <div className="form-group">
+    //               <input type="submit" value="Login" className="btn float-right login_btn" disabled={isFetching} />
 
     //             </div>
     //           </form>
     //         </div>
-    //         <div class="card-footer">
-    //           <div class="d-flex justify-content-center links">
+    //         <div className="card-footer">
+    //           <div className="d-flex justify-content-center links">
     //             Don't have an account?<a href="#">Sign Up</a>
     //           </div>
-    //           <div class="d-flex justify-content-center">
+    //           <div className="d-flex justify-content-center">
     //             <a href="#">Forgot your password?</a>
     //           </div>
     //         </div>
@@ -113,54 +115,54 @@ export default function Login() {
     //     </div>
     //   </div>
     // </div>
-    // <div class="login-wrap">
-    //   <div class="login-html">
-    //     <input id="tab-1" type="radio" name="tab" class="sign-in" checked /><label for="tab-1" class="tab">Sign In</label>
-    //     <input id="tab-2" type="radio" name="tab" class="sign-up" /><label for="tab-2" class="tab">Sign Up</label>
-    //     <div class="login-form">
-    //       <div class="sign-in-htm">
-    //         <div class="group">
-    //           <label for="user" class="label">Username</label>
-    //           <input id="user" type="text" class="input" />
+    // <div className="login-wrap">
+    //   <div className="login-html">
+    //     <input id="tab-1" type="radio" name="tab" className="sign-in" checked /><label for="tab-1" className="tab">Sign In</label>
+    //     <input id="tab-2" type="radio" name="tab" className="sign-up" /><label for="tab-2" className="tab">Sign Up</label>
+    //     <div className="login-form">
+    //       <div className="sign-in-htm">
+    //         <div className="group">
+    //           <label for="user" className="label">Username</label>
+    //           <input id="user" type="text" className="input" />
     //         </div>
-    //         <div class="group">
-    //           <label for="pass" class="label">Password</label>
-    //           <input id="pass" type="password" class="input" data-type="password" />
+    //         <div className="group">
+    //           <label for="pass" className="label">Password</label>
+    //           <input id="pass" type="password" className="input" data-type="password" />
     //         </div>
-    //         <div class="group">
-    //           <input id="check" type="checkbox" class="check" checked />
-    //           <label for="check"><span class="icon"></span> Keep me Signed in</label>
+    //         <div className="group">
+    //           <input id="check" type="checkbox" className="check" checked />
+    //           <label for="check"><span className="icon"></span> Keep me Signed in</label>
     //         </div>
-    //         <div class="group">
-    //           <input type="submit" class="button" value="Sign In" />
+    //         <div className="group">
+    //           <input type="submit" className="button" value="Sign In" />
     //         </div>
-    //         <div class="hr"></div>
-    //         <div class="foot-lnk">
+    //         <div className="hr"></div>
+    //         <div className="foot-lnk">
     //           <a href="#forgot">Forgot Password?</a>
     //         </div>
     //       </div>
-    //       <div class="sign-up-htm">
-    //         <div class="group">
-    //           <label for="user" class="label">Username</label>
-    //           <input id="user" type="text" class="input" />
+    //       <div className="sign-up-htm">
+    //         <div className="group">
+    //           <label for="user" className="label">Username</label>
+    //           <input id="user" type="text" className="input" />
     //         </div>
-    //         <div class="group">
-    //           <label for="pass" class="label">Password</label>
-    //           <input id="pass" type="password" class="input" data-type="password" />
+    //         <div className="group">
+    //           <label for="pass" className="label">Password</label>
+    //           <input id="pass" type="password" className="input" data-type="password" />
     //         </div>
-    //         <div class="group">
-    //           <label for="pass" class="label">Repeat Password</label>
-    //           <input id="pass" type="password" class="input" data-type="password" />
+    //         <div className="group">
+    //           <label for="pass" className="label">Repeat Password</label>
+    //           <input id="pass" type="password" className="input" data-type="password" />
     //         </div>
-    //         <div class="group">
-    //           <label for="pass" class="label">Email Address</label>
-    //           <input id="pass" type="text" class="input" />
+    //         <div className="group">
+    //           <label for="pass" className="label">Email Address</label>
+    //           <input id="pass" type="text" className="input" />
     //         </div>
-    //         <div class="group">
-    //           <input type="submit" class="button" value="Sign Up" />
+    //         <div className="group">
+    //           <input type="submit" className="button" value="Sign Up" />
     //         </div>
-    //         {/* <div class="hr"></div> */}
-    //         {/* <div class="foot-lnk">
+    //         {/* <div className="hr"></div> */}
+    //         {/* <div className="foot-lnk">
     //       <label for="tab-1">Already Member?</a>
     //     </div> */}
     //       </div>
@@ -169,49 +171,54 @@ export default function Login() {
     // </div>
 
 
-    <div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
-      <div class="card card0 border-0">
-        <div class="row d-flex">
-          <div class="col-lg-6">
-            <div class="card1 pb-5">
-              <div class="row"> <img src={Logo} class="logo" /> </div>
-              <div class="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="https://i.imgur.com/uNGdWHi.png" class="image" /> </div>
+    <div className="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+      <form onSubmit={handleClick} >
+        <div className="card card0 border-0">
+          <div className="row d-flex">
+            <div className="col-lg-6">
+              <div className="card1 pb-5">
+                <div className="row"> <img src={Logo} className="logoLogin" /> </div>
+                <div className="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="https://i.imgur.com/uNGdWHi.png" className="image" /> </div>
+              </div>
             </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="card2 card border-0 px-4 py-5">
-              <div class="row mb-4 px-3">
-                <h6 class="mb-0 mr-4 mt-2">Sign in with</h6>
-                <div class="facebook text-center mr-3">
-                  <div class="fa fa-facebook"></div>
-                </div>
-                <div class="twitter text-center mr-3">
-                  <div class="fa fa-twitter"></div>
-                </div>
-                <div class="linkedin text-center mr-3">
-                  <div class="fa fa-linkedin"></div>
-                </div>
-              </div>
-              <div class="row px-3 mb-4">
-                <div class="line"></div> <small class="or text-center">Or</small>
-                <div class="line"></div>
-              </div>
-              <div class="row px-3"> <label class="mb-1">
-                <h6 class="mb-0 text-sm">Email Address</h6>
-              </label> <input class="mb-4" type="text" name="email" placeholder="Enter a valid email address" /> </div>
-              <div class="row px-3"> <label class="mb-1">
-                <h6 class="mb-0 text-sm">Password</h6>
-              </label> <input type="password" name="password" placeholder="Enter password" /> </div>
-              <div class="row px-3 mb-4">
-                <div class="custom-control custom-checkbox custom-control-inline"> <input id="chk1" type="checkbox" name="chk" class="custom-control-input" /> <label for="chk1" class="custom-control-label text-sm">Remember me</label> </div> <a href="#" class="ml-auto mb-0 text-sm">Forgot Password?</a>
-              </div>
-              <div class="row mb-3 px-3"> <button type="submit" class="btn btn-blue text-center">Login</button> </div>
-              <div class="row mb-4 px-3"> <small class="font-weight-bold">Don't have an account? <a class="text-danger ">Register</a></small> </div>
-            </div>
-          </div>
-        </div>
+            <div className="col-lg-6">
+              <div className="card2 card border-0 px-4 py-5">
+                <div className="row mb-4 px-3"><larg className="font-weight-bold">
+                  <h6 className="mb-0 mr-4 mt-2">Sign in with</h6></larg>
+                  <div className="facebook text-center mr-3">
 
-      </div>
+                    <div className="fa fa-facebook"></div>
+                  </div>
+                  <div className="twitter text-center mr-3">
+                    <div className="fa fa-google"></div>
+
+                  </div>
+
+                </div>
+                <div className="row px-3 mb-4">
+                  <div className="line"></div> <small className="or text-center">Or</small>
+                  <div className="line"></div>
+                </div>
+                <div className="row px-3"> <label className="mb-1">
+                  <h6 className="mb-0 text-sm">User name</h6>
+                </label> <input className="mb-4" type="text" name="username" placeholder="Enter Username" required ref={username} /> </div>
+                <div className="row px-3"> <label className="mb-1">
+                  <h6 className="mb-0 text-sm">Password</h6>
+                </label> <input type="password" name="password" placeholder="Enter password"
+                  required
+                  minLength="6"
+                  ref={password} /> </div>
+                <div className="row px-3 mb-4">
+                  <div className="custom-control custom-checkbox custom-control-inline"> <input id="chk1" type="checkbox" name="chk" className="custom-control-input" /> <label for="chk1" className="custom-control-label text-sm">Remember me</label> </div>
+                </div>
+                <div className="row mb-3 px-3" ><Link to='home'> <button type="submit" className="btn btn-blue text-center" disabled={isFetching} >Login</button></Link> </div>
+                <div className="row mb-4 px-3"> <larg className="font-weight-bold">Don't have an account? <Link className="text-danger " to="/register">Register</Link> </larg> </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </form>
     </div>
 
   );
--- swapo/src/pages/register/Register.jsx
+++ swapo/src/pages/register/Register.jsx
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { useRef } from "react";
+import { Link } from 'react-router-dom';
+import Logo from '../login/logo.png'
+
+
 import "./register.css";
 import { useHistory } from "react-router";
 
@@ -22,7 +26,7 @@ export default function Register() {
       try {
         await axios.post("/signup", user);
         history.push("/login");
-        console.log('user',user);
+        console.log('user', user);
       } catch (err) {
         console.log(err);
       }
@@ -30,51 +34,118 @@ export default function Register() {
   };
 
   return (
-    <div className="login">
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className="loginLogo">Swapo</h3>
-          <span className="loginDesc">
-          Web app that connects many service providers and those who give their services to each other by swap their knowledge and expertise.
-          </span>
+    // <div className="login">
+    //   <div className="loginWrapper">
+    //     <div className="loginLeft">
+    //       <h3 className="loginLogo">Swapo</h3>
+    //       <span className="loginDesc">
+    //         Web app that connects many service providers and those who give their services to each other by swap their knowledge and expertise.
+    //       </span>
+    //     </div>
+    //     <div className="loginRight">
+    //       <form className="loginBox" onSubmit={handleClick}>
+    //         <input
+    //           placeholder="Username"
+    //           required
+    //           ref={username}
+    //           className="loginInput"
+    //         />
+    //         <input
+    //           placeholder="Email"
+    //           required
+    //           ref={email}
+    //           className="loginInput"
+    //           type="email"
+    //         />
+    //         <input
+    //           placeholder="Password"
+    //           required
+    //           ref={password}
+    //           className="loginInput"
+    //           type="password"
+    //           minLength="6"
+    //         />
+    //         <input
+    //           placeholder="Password Again"
+    //           required
+    //           ref={passwordAgain}
+    //           className="loginInput"
+    //           type="password"
+    //         />
+    //         <button className="loginButton" type="submit">
+    //           Sign Up
+    //         </button>
+    //         <button className="loginRegisterButton">Log into Account</button>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+      <form onSubmit={handleClick} >
+        <div className="card card0 border-0">
+          <div className="row d-flex">
+            <div className="col-lg-6">
+              <div className="card1 pb-5">
+                <div className="row"> <img src={Logo} className="logoLogin" /> </div>
+                <div className="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="https://i.imgur.com/uNGdWHi.png" className="image" /> </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="card2 card border-0 px-4 py-5">
+                <div className="row mb-4 px-3"><larg className="font-weight-bold">
+                  <h6 className="mb-0 mr-4 mt-2">Sign Up with</h6></larg>
+                  <div className="facebook text-center mr-3">
+
+                    <div className="fa fa-facebook"></div>
+                  </div>
+                  <div className="twitter text-center mr-3">
+                    <div className="fa fa-google"></div>
+
+                  </div>
+
+                </div>
+                <div className="row px-3 mb-4">
+                  <div className="line"></div> <small className="or text-center">Or</small>
+                  <div className="line"></div>
+                </div>
+                <div className="row px-3"> <label className="mb-1">
+                  {/* <h6 className="mb-0 text-sm">Email</h6> */}
+                </label> <input className="mb-4" type="email" name="email" placeholder="Enter a valid Email" required ref={email} /> </div>
+
+                <div className="row px-3"> <label className="mb-1">
+                  {/* <h6 className="mb-0 text-sm">User name</h6> */}
+                </label> <input className="mb-4" type="text" name="username" placeholder="Enter Username" required ref={username} /> </div>
+
+
+
+                <div className="row px-3"> <label className="mb-1">
+                  {/* <h6 className="mb-0 text-sm">Password</h6> */}
+                </label> <input type="password" name="password" placeholder="Enter password"
+                  required
+                  minLength="6"
+                  ref={password} /> </div>
+
+                <div className="row px-3 last"> <label className="mb-1">
+                  {/* <h6 className="mb-0 text-sm">Confirmed Password</h6> */}
+                </label> <input type="password" name="passwordAgain" placeholder="Confirmed Password "
+                  required
+                  minLength="6"
+                  ref={passwordAgain} /> </div>
+
+                <div className="row px-3 mb-4">
+                  <div className="custom-control custom-checkbox custom-control-inline"> <input id="chk1" type="checkbox" name="chk" className="custom-control-input" /> <label for="chk1" className="custom-control-label text-sm">Remember me</label> </div>
+                </div>
+                <div className="row mb-3 px-3" ><Link to=''> <button type="submit" className="btn btn-blue text-center" >Sign up</button></Link> </div>
+                <div className="row mb-4 px-3"> <larg className="font-weight-bold">You have an account? <Link className="text-danger " to="/login">Login</Link> </larg> </div>
+              </div>
+            </div>
+          </div>
+
         </div>
-        <div className="loginRight">
-          <form className="loginBox" onSubmit={handleClick}>
-            <input
-              placeholder="Username"
-              required
-              ref={username}
-              className="loginInput"
-            />
-            <input
-              placeholder="Email"
-              required
-              ref={email}
-              className="loginInput"
-              type="email"
-            />
-            <input
-              placeholder="Password"
-              required
-              ref={password}
-              className="loginInput"
-              type="password"
-              minLength="6"
-            />
-            <input
-              placeholder="Password Again"
-              required
-              ref={passwordAgain}
-              className="loginInput"
-              type="password"
-            />
-            <button className="loginButton" type="submit">
-              Sign Up
-            </button>
-            <button className="loginRegisterButton">Log into Account</button>
-          </form>
-        </div>
-      </div>
+      </form>
     </div>
+
+
+
   );
 }
